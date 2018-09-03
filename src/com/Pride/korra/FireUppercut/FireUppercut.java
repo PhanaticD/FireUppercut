74bcb46ea6ea36b904c9755aceb3cf02ee14902f
Josh <javw.phanatic@gmail.com>
2018-09-03 12:11:40 -0400

    some tweaks

--- src/com/Pride/korra/FireUppercut/FireUppercut.java
+++ src/com/Pride/korra/FireUppercut/FireUppercut.java
@@ -22,11 +22,11 @@ public class FireUppercut extends FireAbility implements AddonAbility {
 	private int fireTicks;
 	private double radius;
 	
-	private Location location;
+	private Location particleStartLoc, particleEndLoc;
 
 	public FireUppercut(Player player) {
 		super(player);
-		// TODO Auto-generated constructor stub
+
 		if (!bPlayer.canBend(this)) {
 			return;
 		}
@@ -34,105 +34,104 @@ public class FireUppercut extends FireAbility implements AddonAbility {
 		damage = ConfigManager.getConfig().getDouble(path + "Damage");
 		fireTicks = ConfigManager.getConfig().getInt(path + "FireTicks");
 		radius = ConfigManager.getConfig().getDouble(path + "Radius");
-		location = GeneralMethods.getRightSide(player.getLocation(), .55).add(0, 0.8, 0);
-		start();
+
+		particleStartLoc = GeneralMethods.getRightSide(player.getLocation(), .55).add(0, 0.8, 0);
+		
+		for (Entity entity : GeneralMethods.getEntitiesAroundPoint(particleStartLoc, radius)) {
+			if (entity instanceof LivingEntity && entity.getEntityId() != player.getEntityId()) {
+                            DamageHandler.damageEntity(entity, damage, this);
+                            entity.setFireTicks(fireTicks);
+                            entity.setVelocity(entity.getVelocity().add(new Vector(0, 1.1, 0)));
+			}
+		}
+                player.setVelocity(player.getVelocity().add(new Vector(0, 0.5, 0)));
+                
+                particleEndLoc = GeneralMethods.getRightSide(player.getLocation(), .55).add(0, 5, 0);
+                
+                start();
+                bPlayer.addCooldown(this);
 	}
 
 	@Override
 	public long getCooldown() {
-		// TODO Auto-generated method stub
 		return cooldown;
 	}
 
 	@Override
 	public Location getLocation() {
-		// TODO Auto-generated method stub
-		return location;
+		return particleStartLoc;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "FireUppercut";
 	}
 
 	@Override
 	public boolean isHarmlessAbility() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isSneakAbility() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public void progress() {
-		// TODO Auto-generated method stub
+		if (player.isDead() || !player.isOnline()) {
+                        bPlayer.addCooldown(this);
+                        remove();
+			return;
+		}
 		if (!bPlayer.canBendIgnoreBindsCooldowns(this)) {
 			bPlayer.addCooldown(this);
 			remove();
 			return;
 		}
-		if (player.isDead() || !player.isOnline()) {
-			remove();
-			return;
-		}
-		Location newLoc = GeneralMethods.getRightSide(player.getLocation(), .55).add(0, 5, 0);
-		Vector dir = GeneralMethods.getDirection(location, newLoc);
+		
+		Vector dir = GeneralMethods.getDirection(particleStartLoc, particleEndLoc);
 		dir.normalize().multiply(0.5D);
-		location.add(dir);
+		particleStartLoc.add(dir);
 		
-		if (location.distanceSquared(newLoc) < 0.22 * 0.22) {
-			bPlayer.addCooldown(this);
-			remove();
-			return;
-		}
-		ParticleEffect.FLAME.display(location, 0.2F, 0.2F, 0.2F, 0F, 8);
-		ParticleEffect.CRIT.display(location, 0.2F, 0.2F, 0.2F, 0F, 8);
+		ParticleEffect.FLAME.display(particleStartLoc, 0.2F, 0.2F, 0.2F, 0F, 8);
+		ParticleEffect.CRIT.display(particleStartLoc, 0.2F, 0.2F, 0.2F, 0F, 8);
 		
-		for (Entity entity : GeneralMethods.getEntitiesAroundPoint(location, radius)) {
+		for (Entity entity : GeneralMethods.getEntitiesAroundPoint(particleStartLoc, radius)) {
 			if (entity instanceof LivingEntity && entity.getEntityId() != player.getEntityId()) {
 				ParticleEffect.CRIT.display(entity.getLocation().add(0, 1, 0), 0.3F, 0.3F, 0.3F, 0F, 10);
-				Vector vec = entity.getVelocity();
-				vec.setY(0.7);
-				entity.setVelocity(vec.multiply(1.4));
-				DamageHandler.damageEntity(entity, damage, this);
-				entity.setFireTicks(fireTicks);
 			}
 		}
+		if (particleStartLoc.distanceSquared(particleEndLoc) < 0.1) {
+                        remove();
+			return;
+		}
+
 	}
 
 	@Override
 	public String getAuthor() {
-		// TODO Auto-generated method stub
-		return "Prride";
+		return "Prride & PhanaticD";
 	}
 
 	@Override
 	public String getVersion() {
-		// TODO Auto-generated method stub
 		return "FireUppercut Build 1";
 	}
 	
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
 		return "Uppercuts with a fist of fire. Launches enemies into the air and does fire damage.";
 	}
 
 	@Override
 	public String getInstructions() {
-		// TODO Auto-generated method stub
 		return "Sneak to launch an uppercut attack.";
 	}
 
 	@Override
 	public void load() {
-		// TODO Auto-generated method stub
-		ProjectKorra.plugin.getServer().getPluginManager().registerEvents(new UppercutListener(), ProjectKorra.plugin);
+		ProjectKorra.plugin.getServer().getPluginManager().registerEvents(new FireUppercutListener(), ProjectKorra.plugin);
 		ProjectKorra.log.info(getName() + " " + getVersion() + " by " + getAuthor() + " loaded! ");
 		
 		ConfigManager.getConfig().addDefault(path + "Cooldown", 4500);
@@ -144,7 +143,6 @@ public class FireUppercut extends FireAbility implements AddonAbility {
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
 		ProjectKorra.log.info(getName() + " " + getVersion() + " by " + getAuthor() + " stopped! ");
 		super.remove();
 	}
